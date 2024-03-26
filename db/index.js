dc3f94dcbabb1ea8df29fb12dfb0394cfabe6b46
omkarnirval <65648880+Omkar393@users.noreply.github.com>
2024-03-26 17:03:02 +0530

    changed db connection function

--- db/index.js
+++ db/index.js
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
 
 export const dbConnection = async () => {
-    return mongoose.connect(process.env.MONGO_URl, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-        console.log('Database connected successfully');
-    }).catch((err) => {
-        console.log(err);
-    })
+    try {
+        mongoose.connect(process.env.MONGO_URl).then(() => {
+            console.log('Database connected successfully');
+        }).catch((err) => {
+            console.log(err);
+        })
+    } catch (error) {
+        console.log(error);
+    }
 }
